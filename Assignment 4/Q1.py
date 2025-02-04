c31ed0a470abc8c753a5038c5f475fefd16ab2da
Dhruv Gupta <itsdhruvg@gmail.com>
2025-02-04 11:38:46 +0530

    upload

---
+++ Assignment 4/Q1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("Original Array:", arr)
+
+added_arr = arr + 2
+print("After Addition of 2:", added_arr)
+
+multiplied_arr = arr * 3
+print("After Multiplication by 3:", multiplied_arr)
+
+divided_arr = arr / 2
+print("After Division by 2:", divided_arr)
